fdd5fc5979d7355fcfb8e2289d07de5bf8e04d45
arturcatao <arturcataodoo@gmail.com>
2025-09-26 17:49:53 -0300

    Primeira classe, Position

---
+++ chess/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import board.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Position pos = new Position(5, 0); 
+		
+		System.out.println(pos.toString());
+	}
+
+}
